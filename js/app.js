535869d61f9f25acae33ff62b5475b95259def53
Ezequiel Lino <sonisombra@gmail.com>
2023-05-11 12:11:57 -0600

    changes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Campos del formulario
 const mascotaInput = document.querySelector('#mascota');
 const propietarioInput = document.querySelector('#propietario');
 const telefonoInput = document.querySelector('#telefono');
@@ -5,16 +6,24 @@ const fechaInput = document.querySelector('#fecha');
 const horaInput = document.querySelector('#hora');
 const sintomasInput = document.querySelector('#sintomas');
 
+// UI
 const formulario = document.querySelector('#nueva-cita');
-
 const contenedorCitas = document.querySelector('#citas');
 
+// Registrar eventos
 eventListeners();
 function eventListeners() {
   mascotaInput.addEventListener('change', datosCita);
+  propietarioInput.addEventListener('change', datosCita);
+  telefonoInput.addEventListener('change', datosCita);
+  fechaInput.addEventListener('change', datosCita);
+  horaInput.addEventListener('change', datosCita);
+  sintomasInput.addEventListener('change', datosCita);
 }
 
+// Objeto con la informacion de la cita
 const citaObj = {
+  // name para relacionarlo con los inputs de html
   mascota: '',
   propietario: '',
   telefono: '',
@@ -23,7 +32,9 @@ const citaObj = {
   sintomas: ''
 }
 
+// Funcion para agregar datos al objeto
 function datosCita(e) {
+  // name para relacionarlo con los inputs de html
   citaObj[e.target.name] = e.target.value
   console.log(citaObj);
 }
