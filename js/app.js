8f08e42d70876febacee55e831ddb658aea9a559
Ezequiel Lino <sonisombra@gmail.com>
2023-05-11 11:05:23 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const mascotaInput = document.querySelector('#mascota');
+const propietarioInput = document.querySelector('#propietario');
+const telefonoInput = document.querySelector('#telefono');
+const fechaInput = document.querySelector('#fecha');
+const horaInput = document.querySelector('#hora');
+const sintomasInput = document.querySelector('#sintomas');
+
+const formulario = document.querySelector('#nueva-cita');
+
+const contenedorCitas = document.querySelector('#citas');
+console.log(formulario);
